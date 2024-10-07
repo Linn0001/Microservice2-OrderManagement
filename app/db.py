bd8ccb9e476e72b4ac1f4537ac486fbdcb673f10
Linn <136191464+Linn0001@users.noreply.github.com>
2024-10-06 20:45:58 -0500

    Delete app directory

--- app/db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-# Configuración de la base de datos
-DATABASE_URL = "mysql://oli:perrito0linn@localhost:3306/modulo2"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
